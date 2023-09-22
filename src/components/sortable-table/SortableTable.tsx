490fc6ceb3fda197d55783a5ffd3ca132f8b64fd
pure-js <3690368+pure-js@users.noreply.github.com>
2023-09-22 22:35:16 +0400

    feat: add label and table

---
+++ src/components/sortable-table/SortableTable.tsx
@@ -0,0 +1,28 @@
+export const SortableTable = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Title</th>
+          <th>Amount</th>
+          <th>Distance</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>15.12.2021</td>
+          <td>Bremen</td>
+          <td>2</td>
+          <td>456 km</td>
+        </tr>
+        <tr>
+          <td>16.12.2002</td>
+          <td>Leipzig</td>
+          <td>6</td>
+          <td>120 km</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
