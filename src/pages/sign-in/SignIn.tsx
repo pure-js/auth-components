4f15d7c6023e0df9a058f7a1a7634af068c8cc9e
Arthur Green <curious.dev@icloud.com>
2023-10-14 21:58:52 +0400

    BREAKING CHANGE: delete label component

--- src/pages/sign-in/SignIn.tsx
+++ src/pages/sign-in/SignIn.tsx
@@ -3,16 +3,13 @@ import { Route } from '@tanstack/react-router';
 import { Form } from '@/components/form';
 import { Input } from '@/components/input';
 import { Button } from '@/components/button';
-import { Label } from '@/components/label/Label';
 import { rootRoute } from '@/pages/__root';
 
 export const SignInPage = () => {
   return (
     <Form onSubmit={() => {}}>
-      <Label label="Username" htmlFor="username" />
-      <Input type="text" id="username" autoFocus={true} />
-      <Label label="Password" htmlFor="password" />
-      <Input type="password" minLength={8} id="password" />
+      <Input label="Username" type="text" id="username" autoFocus={true} />
+      <Input label="Password" type="password" minLength={8} id="password" />
       <Button primary size="large" onClick={() => {}} label="Log in" />
     </Form>
   );
