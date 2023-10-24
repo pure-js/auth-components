96f00729d5559242ba128391aa1ef3eb27f3c924
Arthur Green <curious.dev@icloud.com>
2023-10-24 22:20:13 +0400

    docs: add SortableTable story

--- src/components/form/Form.stories.tsx
+++ src/components/form/Form.stories.tsx
@@ -14,7 +14,7 @@ const meta = {
   tags: ['autodocs'],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
+    layout: 'centered',
   },
 } satisfies Meta<typeof Form>;
 
