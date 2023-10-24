79569d4d89ffc35d22fce9982989a9f3d8499372
Arthur Green <curious.dev@icloud.com>
2023-10-24 23:43:49 +0400

    docs: add SortableTable story (#25)

---
+++ src/components/sortable-table/SortableTable.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { SortableTable } from './SortableTable';
+
+const meta = {
+  title: 'SortableTable',
+  component: SortableTable,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered',
+  },
+} satisfies Meta<typeof SortableTable>;
+
+export default meta;
+type Story = StoryObj<typeof SortableTable>;
+
+// const SortableTableTemplate: Story = {
+//   render: ({ ...args }) => {
+//     return <SortableTable {...args} />;
+//   },
+// };
+
+// export const SortableTableBasic: Story = {
+//   ...SortableTableTemplate,
+//   args: {},
+// };
+
+export const SortableTableBasic: Story = {
+  args: {},
+};
