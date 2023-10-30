3e1fc765da4832b97d1eb0fbd36f0dcfd967b3c1
Arthur Green <curious.dev@icloud.com>
2023-10-30 13:07:05 +0400

    build: fix prod build for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/auth-components/' : '/',
   plugins: [react()],
   resolve: {
     alias: [
