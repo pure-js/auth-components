a5d5c1c5ed7d6a3ab6b111e184e045924cac96e4
Arthur Green <curious.dev@icloud.com>
2024-03-24 21:39:37 +0530

    refactor: from css modules to vanilla-extract css (#83)
    * refactor: from css modules to css in js
    
    * docs: change types in stories
    
    * chore: ignore generated files
    
    * ci: up e2e workflow
    
    * build: up storybook
    
    * build: uninstalla few dependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,10 +3,10 @@ import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-a11y',
+    '@storybook/addon-onboarding',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
     '@storybook/addon-interactions',
   ],
   framework: {
